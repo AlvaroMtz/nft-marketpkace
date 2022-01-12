26ca43ee80676f646a7b3946c36308d4305d61aa
AlvaroMtz <hola@alvaromtz.es>
2022-01-12 19:17:39 +0100

    add NFT metadata

--- contracts/AmzPunks.sol
+++ contracts/AmzPunks.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
+import "./Base64.sol";
+
 contract AmzPunks is ERC721, ERC721Enumerable, PaymentSplitter {
     using Counters for Counters.Counter;
 
@@ -24,6 +26,27 @@ contract AmzPunks is ERC721, ERC721Enumerable, PaymentSplitter {
         _safeMint(msg.sender, current);
         _idCounter.increment();
     }
+
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        override 
+        returns(string memory) {
+            require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token");
+
+            string memory jsonURI = Base64.encode(
+                abi.encodePacked(
+                    '{ "name": "AmZPunks #',
+                    tokenId,
+                    '", "description": "AmZ Punks are randomized Avatars stored on chain to teach DApp development on Platzi", "imagen": "',
+                    "// TODO: Calculate image URL"
+                    '"}'
+                )
+            );
+
+            return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
+
         // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
